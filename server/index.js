294ea038ae158219ec245552bcc7f582c7049d2c
Saleemahmh <ssaleemah24@gmail.com>
2025-09-04 13:01:01 +0530

    API url changed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const authRoutes = require("./routes/auth");
 const listingRoutes = require("./routes/listing");
 const bookingRoutes = require("./routes/booking.js");
 const userRoutes = require("./routes/user.js");
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://dreamhaven-app-13dz.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -20,7 +25,6 @@ app.use("/bookings", bookingRoutes);
 app.use("/users", userRoutes);
 /* Moongoose setup */
 
-const PORT = 5000;
 mongoose
   .connect(process.env.MONGO_URL, {
     dbName: "dreamhavendb",
@@ -28,6 +32,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+    app.listen(process.env.PORT || 5000, "0.0.0.0", () =>
+      console.log(`Server Port : ${PORT}`)
+    );
   })
   .catch((err) => console.log(`${err} did not connect`));
