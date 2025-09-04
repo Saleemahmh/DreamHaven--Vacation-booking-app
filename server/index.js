decfdc026b733976ef905d182b934c58cfba90c0
Saleemahmh <ssaleemah24@gmail.com>
2025-09-04 13:24:33 +0530

    Further changes for deployment.

--- server/index.js
+++ server/index.js
@@ -8,15 +8,11 @@ const authRoutes = require("./routes/auth");
 const listingRoutes = require("./routes/listing");
 const bookingRoutes = require("./routes/booking.js");
 const userRoutes = require("./routes/user.js");
-app.use(
-  cors({
-    origin: "https://dreamhaven-app-13dz.onrender.com",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+const PORT = process.env.PORT || 5000;
 /*Routes */
 
 app.use("/auth", authRoutes);
@@ -32,8 +28,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(process.env.PORT || 5000, "0.0.0.0", () =>
-      console.log(`Server Port : ${PORT}`)
-    );
+    app.listen(PORT, "0.0.0.0", () => console.log(`Server Port : ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
